c4b11531f8dbff37230ee2363cf079d84422b4b0
Jasper Boeijenga <jboeijenga@gmail.com>
2020-12-02 12:56:12 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AsyncState, Status, toData } from '../async/async.model';
 import { Component } from '@angular/core';
+import { Data } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'async-component';
+  public state: AsyncState<Data> = {
+    status: Status.PENDING
+  };
+
+  public toData = toData;
+
+  public setData(): void {
+    this.state = {
+      status: Status.SUCCESS,
+      data: {
+        id: '1',
+        description: 'This is a description of an item',
+        address: {
+          postcode: '1234 AB',
+          houseNumber: '1'
+        }
+      }
+    };
+  }
+
+  public setError(): void {
+    this.state = {
+      status: Status.ERROR,
+      data: undefined,
+      error: 'Things are going wrong...'
+    };
+  }
+
+  public unsetData(): void {
+    this.state = {
+      status: Status.PENDING,
+      data: undefined
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AsyncModule } from '../async/async.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AsyncModule
   ],
   providers: [],
   bootstrap: [AppComponent]
