39c38469e0a92f381612321355abf1ecbad50ac0
Marloes van de Wouw <marloes.van.bragt@essent.nl>
2022-07-21 15:48:14 +0200

    change appmodule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { CoreModule } from './core/core.module';
-import { Leegstand } from './customer/leegstand/leegstand.component';
+import { Leegstand } from './leegstand/leegstand.component';
 @NgModule({
   declarations: [
     AppComponent,
