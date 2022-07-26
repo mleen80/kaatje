944d5a5161305f4fcc3fe5e57540f4fda117c755
Marloes van de Wouw <marloes.van.bragt@essent.nl>
2022-07-26 14:47:18 +0200

    add router to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
-constructor() {}
+constructor( private router: Router,
+  private route: ActivatedRoute
+  ) {}
 
+
+  ngOnInit() {
+
+  }
 }
