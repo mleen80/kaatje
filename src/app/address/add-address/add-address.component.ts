a53c84a27c5e4370dd67a6bf201c5c0c21c450a5
Marleen van Dijk <marleen_van_dijk@essent.nl>
2022-10-04 17:11:56 +0200

    validaties formulier toegevoegd

--- src/app/address/add-address/add-address.component.ts
+++ src/app/address/add-address/add-address.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { minLength } from 'ngrx-forms/validation';
 
 @Component({
   selector: 'app-add-address',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-address.component.css']
 })
 export class AddAddressComponent implements OnInit {
+  addAddress!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.addAddress = new FormGroup({
+      'straat': new FormControl(null, Validators.required),
+      'huisnummer': new FormControl(null, [Validators.required, Validators.pattern("^(0|[1-9][0-9]*)$")]),
+      'toevoeging': new FormControl(null),
+      'postcode': new FormControl(null, Validators.required),
+      'woonplaats': new FormControl(null, Validators.required),
+      'eanstroom': new FormControl(null, [Validators.required, Validators.minLength(18)]),
+      'eangas': new FormControl(null, [Validators.required, Validators.minLength(18)]),
+      'startdatum': new FormControl(null, Validators.required)
+    })
   }
+
 
 }
