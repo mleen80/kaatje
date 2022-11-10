ed6303bb2990ac1433e7b3fbc5a1474bfc702162
Marloes van de Wouw <marloes.van.bragt@essent.nl>
2022-11-10 12:02:21 +0100

    fixed accordion

--- src/app/address/list-addresses/list-addresses.component.ts
+++ src/app/address/list-addresses/list-addresses.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Address } from 'src/app/api/address/address';
 import { AddressItemComponent } from 'src/app/address/address-item/address-item.component';
+import { uniqueId } from 'lodash'
 
 @Component({
   selector: 'app-list-addresses',
@@ -8,6 +9,7 @@ import { AddressItemComponent } from 'src/app/address/address-item/address-item.
   styleUrls: ['./list-addresses.component.css'],
 })
 export class ListAddressesComponent implements OnInit {
+  componentId = uniqueId('list-address-accordion-')
   @Input() addresses!: Address[];
   status = ['Active', 'Future', 'Past'];
   panelOpenState = false;
